813f65baa68405611a7e4d59b3dc79b47a3a0b29
Karol Dobrek <Inakus@wp.pl>
2022-12-05 19:49:01 +0100

    connected to database and made model and schema

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import * as dotenv from 'dotenv'
 
 dotenv.config()
@@ -8,6 +9,15 @@ const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 8080;
+
+async function main() {
+  await mongoose.connect(`${process.env.MONGODB_LINK}`)
+  console.log("Connected to MongoDB");
+}
+
+main().catch((err) => {
+  console.error(err);
+});
 
 app.get("/", (req, res) => {
   res.json("Hello from server");
